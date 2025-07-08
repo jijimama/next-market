a6b81eb5d14dea3af17248f6c6e48775505905fa
jijimama <ninisesekoma@gmail.com>
2025-07-08 12:16:29 +0900

    apiを追加

---
+++ app/api/item/create/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server"
+
+export async function POST(request) {
+    console.log(await request.json())
+    return NextResponse.json({
+        message: "アイテム作成",
+    })
+}
